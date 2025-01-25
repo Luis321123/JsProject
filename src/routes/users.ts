69eba84315232b8c5a39125ef1143ba46add82c3
Louis Aurea <louis@nextstation.cl>
2025-01-24 21:20:17 -0400

    get all users func

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,8 @@
 import express from 'express';
 
 import { getAllUsers } from '../controllers/users';
-import { register, login, update } from '../controllers/authentication';
 
 export default (router: express.Router) => {
-    router.get('/users', getAllUsers),
-    router.post('/users', register);
-    router.put('/users', update);
-    router.post('/users/login', login);
+    router.get('/users', getAllUsers)
 }
 
